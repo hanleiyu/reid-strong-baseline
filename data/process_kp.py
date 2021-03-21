5ea1e150dfdce713343a91d49ba5a5502137772a
hanleiyu <601901861@qq.com>
2021-03-21 11:35:24 +0800

    add process_kp

--- data/process_kp.py
+++ data/process_kp.py
@@ -12,15 +12,16 @@ def get_json_data(json_path, h, w):
         params = json.load(f)
         # 加载json文件中的内容给params
         pose = params['People'][0]['Pose2d']
-        print("before", pose)
+        # print("before", pose)
         # 修改内容
         for i in range(0,25):
-            pose[3*i] /= h
-            pose[3*i + 1] /=w
+            pose[3*i] *= h
+            pose[3*i + 1] *=w
+            pose[3 * i] = float('%.6f'%pose[3 * i])
+            pose[3 * i + 1] = float('%.6f'%pose[3 * i + 1])
         # 打印
-        print("after", pose)
+        # print("after", pose)
     f.close()
-    # 关闭json读模式
 
     return pose
 
@@ -36,20 +37,23 @@ def write_json_data(json_path, pose):
 
 data_path = "/home/yhl/data/VC/"
 
-# img_paths = glob.glob(osp.join(data_path, 'train/*.jpg'))
-# for img_path in img_paths:
-#     img = Image.open(img_path)
-#     w = 128/img.size[0]
-#     h = 256/img.size[1]
-#     json_path = osp.join(data_path, 'kp', img_path[-23:-4]+'_keypoints.json')
-#     with open(json_path,'rb') as f:
-#         write_json_data(json_path, get_json_data(json_path, h, w))
-img_path = "/home/yhl/data/VC/train/0002-01-02-01.jpg"
-img = Image.open(img_path)
-w = 128/img.size[0]
-h = 256/img.size[1]
-print(h, w)
-json_path = osp.join(data_path, 'kp', img_path[-23:-4]+'_keypoints.json')
-with open(json_path,'rb') as f:
-    write_json_data(json_path, get_json_data(json_path, h, w))
+img_paths = glob.glob(osp.join(data_path, 'train/*.jpg'))
+img_paths.append(glob.glob(osp.join(data_path, 'gallery/*.jpg')))
+img_paths.append(glob.glob(osp.join(data_path, 'query/*.jpg')))
+for img_path in img_paths:
+    img = Image.open(img_path)
+    w = 128/img.size[0]
+    h = 256/img.size[1]
+    json_path = osp.join(data_path, 'kp', img_path[-23:-4]+'_keypoints.json')
+    with open(json_path,'rb') as f:
+        write_json_data(json_path, get_json_data(json_path, h, w))
+
+# img_path = "/home/yhl/data/VC/train/0002-01-02-01.jpg"
+# img = Image.open(img_path)
+# w = 128/img.size[0]
+# h = 256/img.size[1]
+# print(img.size)
+# json_path = osp.join(data_path, 'kp', img_path[-23:-4]+'_keypoints.json')
+# with open(json_path,'rb') as f:
+#     write_json_data(json_path, get_json_data(json_path, h, w))
 
