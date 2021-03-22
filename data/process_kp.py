44453f29867ee85dc1ce73171abee5bf3534caf6
hanleiyu <601901861@qq.com>
2021-03-22 13:35:17 +0800

    remove pic&kp

--- data/process_kp.py
+++ data/process_kp.py
@@ -4,6 +4,7 @@ import glob
 import json
 from PIL import Image
 import os.path as osp
+import os
 
 
 def get_json_data(path, h, w):
@@ -11,7 +12,7 @@ def get_json_data(path, h, w):
         params = json.load(f)
         if len(params['people']) < 1:
             pose = []
-            print(json_path)
+            print(path)
         else:
             pose = params['people'][0]['pose_keypoints_2d']
             # print("before", pose)
@@ -32,60 +33,50 @@ def write_json_data(path, pose):
     r.close()
 
 
+def resize_kp(a, b, name):
+    img_paths = glob.glob(osp.join(data_path, name, '*.jpg'))
+    for img_path in img_paths:
+        img = Image.open(img_path)
+        w = a / img.size[0]
+        h = b / img.size[1]
+        json_path = osp.join(data_path, 'kp', name, img_path[-17:-4] + '_keypoints.json')
+        with open(json_path, 'rb') as f:
+            write_json_data(json_path, get_json_data(json_path, h, w))
+
+
+def remove(name, threshold):
+    json_paths = glob.glob(osp.join(data_path, 'kp', name, '*.json'))
+    kp = [1, 2, 5, 8]
+    num = 0
+    for p in json_paths:
+        flag = 1
+        with open(p, 'rb') as f:
+            params = json.load(f)
+            for i in range(len(kp)):
+                if params[3 * kp[i] + 2] <= threshold:
+                    flag = 0
+                    break
+            if (params[3 * 10 + 2] <= threshold or params[3 * 11 + 2] <= threshold) and (
+                    params[3 * 13 + 2] <= threshold or params[3 * 14 + 2] <= threshold):
+                flag = 0
+        f.close()
+        if flag:
+            num += 1
+        else:
+            # print(p)
+            os.remove(p)
+            os.remove(osp.join(data_path, name, p[-28:-15] + '.jpg'))
+    print(num)
+
+
 data_path = "/home/yhl/data/VC/"
 
-json_paths = glob.glob(osp.join(data_path, 'kp/query/*.json'))
-# num = [0 for _ in range(25)]
-kp = [1, 2, 5, 8, 13, 14]
-num = 0
-for p in json_paths:
-    with open(p, 'rb') as f:
-        flag = 1
-        params = json.load(f)
-        for i in range(len(kp)):
-            if params[3 * kp[i] + 2] <= 0.45:
-                flag = 0
-                break
-        if flag: num +=1
-    f.close()
-print(num)
-# for i in range(25):
-#     if num[i]<1000:
-#         print(i, num[i])
 
+# resize_kp(8, 16, "train")
+# resize_kp(8, 16, "gallery")
+# resize_kp(8, 16, "query")
+# os.remove("/home/yhl/data/VC/kp/train/0338-04-03-08_keypoints.json")
 
-# img_paths = glob.glob(osp.join(data_path, 'train/*.jpg'))
-# for img_path in img_paths:
-#     img = Image.open(img_path)
-#     w = 128 / img.size[0]
-#     h = 256 / img.size[1]
-#     json_path = osp.join(data_path, 'kp/train', img_path[-17:-4] + '_keypoints.json')
-#     with open(json_path, 'rb') as f:
-#         write_json_data(json_path, get_json_data(json_path, h, w))
-#
-# img_paths = glob.glob(osp.join(data_path, 'gallery/*.jpg'))
-# for img_path in img_paths:
-#     img = Image.open(img_path)
-#     w = 128 / img.size[0]
-#     h = 256 / img.size[1]
-#     json_path = osp.join(data_path, 'kp/gallery', img_path[-17:-4] + '_keypoints.json')
-#     with open(json_path, 'rb') as f:
-#         write_json_data(json_path, get_json_data(json_path, h, w))
-#
-# img_paths = glob.glob(osp.join(data_path, 'query/*.jpg'))
-# for img_path in img_paths:
-#     img = Image.open(img_path)
-#     w = 128 / img.size[0]
-#     h = 256 / img.size[1]
-#     json_path = osp.join(data_path, 'kp/query', img_path[-17:-4] + '_keypoints.json')
-#     with open(json_path, 'rb') as f:
-#         write_json_data(json_path, get_json_data(json_path, h, w))
-
-# img_path = "/home/yhl/data/VC/train/0338-04-03-08.jpg"
-# img = Image.open(img_path)
-# w = 128/img.size[0]
-# h = 256/img.size[1]
-# print(img.size)
-# json_path = osp.join(data_path, 'kp', img_path[-23:-4]+'_keypoints.json')
-# with open(json_path,'rb') as f:
-#     write_json_data(json_path, get_json_data(json_path, h, w))
+# remove("train", 0.25)
+# remove("gallery", 0.25)
+# remove("query", 0.25)
