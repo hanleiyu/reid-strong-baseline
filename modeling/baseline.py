d16de6e6ce8e45e4a7cdad78c7e577c955c1d76d
hanleiyu <601901861@qq.com>
2021-03-31 21:46:32 +0800

    add unet code

--- modeling/baseline.py
+++ modeling/baseline.py
@@ -10,6 +10,7 @@ from torch import nn
 from .backbones.resnet import ResNet, BasicBlock, Bottleneck
 from .backbones.senet import SENet, SEResNetBottleneck, SEBottleneck, SEResNeXtBottleneck
 from .backbones.resnet_ibn_a import resnet50_ibn_a
+from .backbones.unet_model import UNet
 import numpy as np
 
 def weights_init_kaiming(m):
@@ -271,6 +272,85 @@ class Part(nn.Module):
                               last_stride=last_stride)
         elif model_name == 'resnet50_ibn_a':
             self.base = resnet50_ibn_a(last_stride)
+
+        if pretrain_choice == 'imagenet':
+            self.base.load_param(model_path)
+            print('Loading pretrained ImageNet model......')
+
+        # self.gap = nn.AdaptiveAvgPool2d((256, 128))
+        self.gap = nn.AdaptiveMaxPool2d(1)
+        self.num_classes = num_classes
+        self.neck = neck
+        self.neck_feat = neck_feat
+
+        if self.neck == 'no':
+            self.classifier = nn.Linear(self.in_planes, self.num_classes)
+            # self.classifier = nn.Linear(self.in_planes, self.num_classes, bias=False)     # new add by luo
+            # self.classifier.apply(weights_init_classifier)  # new add by luo
+        elif self.neck == 'bnneck':
+            self.bottleneck = nn.BatchNorm1d(self.in_planes)
+            self.bottleneck.bias.requires_grad_(False)  # no shift
+            self.classifier = nn.Linear(self.in_planes, self.num_classes, bias=False)
+
+            self.bottleneck.apply(weights_init_kaiming)
+            self.classifier.apply(weights_init_classifier)
+
+    def forward(self, x, mask):
+        if self.training:
+            global_feat = self.base(x)
+            # global_feat = nn.functional.interpolate(global_feat, scale_factor=16, mode='nearest')
+            num = len(list(mask[0, :, 0, 0, 0]))
+            feats = [torch.zeros(128, 2048) for _ in range(num + 1)]
+            for i in range(num):
+                feats[i] = self.feat_process(global_feat, mask[:, i, :, :, :])
+            global_feat = self.gap(global_feat)  # (b, 2048, 1, 1)
+            global_feat = global_feat.view(global_feat.shape[0], -1)  # flatten to (bs, 2048)
+            feat = self.bottleneck(global_feat)
+            feats[num] = feat
+            score = [torch.zeros(256) for _ in range(num + 1)]
+            for i in range(num):
+                score[i] = self.classifier(feats[i])
+            score[num] = self.classifier(feat)
+            return score, feats  # global feature for triplet loss
+        else:
+            global_feat = self.base(x)
+            global_feat = self.gap(global_feat)  # (b, 2048, 1, 1)
+            global_feat = global_feat.view(global_feat.shape[0], -1)  # flatten to (bs, 2048)
+
+            if self.neck == 'no':
+                feat = global_feat
+            elif self.neck == 'bnneck':
+                feat = self.bottleneck(global_feat)
+
+            if self.neck_feat == 'after':
+                # print("Test with feature after BN")
+                return feat
+            else:
+                # print("Test with feature before BN")
+                return global_feat
+
+    def load_param(self, trained_path):
+        param_dict = torch.load(trained_path).state_dict()
+        for i in param_dict:
+            if 'classifier' in i:
+                continue
+            self.state_dict()[i].copy_(param_dict[i])
+
+    def feat_process(self, global_feat, mask):
+        feat = torch.mul(global_feat, mask.cuda())
+        feat = feat.mean(2, False)
+        feat = feat.mean(2, False)
+        feat = self.bottleneck(feat)
+        return feat
+
+
+class Decoder(nn.Module):
+    in_planes = 2048
+
+    def __init__(self, num_classes, last_stride, model_path, neck, neck_feat, model_name, pretrain_choice):
+        super(Decoder, self).__init__()
+
+        self.base = UNet(n_channels=3, n_classes=self.num_classes, bilinear=True)
 
         if pretrain_choice == 'imagenet':
             self.base.load_param(model_path)
